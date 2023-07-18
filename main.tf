b3491fdaad5a7f7eb50232285aad753feec9386b
Administrator-at-355997944876 <john.nguyen7744@gmail.com>
2023-07-18 15:22:09 -0400

    change instance type

--- main.tf
+++ main.tf
@@ -1,11 +1,10 @@
 
 resource "aws_instance" "app_server" {
   ami                         = "ami-06ca3ca175f37dd66"
-  instance_type               = "t2.micro"
+  instance_type               = "t2.small"
   vpc_security_group_ids      = ["sg-021c878de93850578"]
   subnet_id                   = "subnet-066e9e8fb8b05eee8" # pulbic subnet 1a
   associate_public_ip_address = true # ask aws to generate a public IP addr
-  
   tags = {
     Name = var.instance_name
   }
